ed6aa20ec20b422985e636b8f21fd8ae36925770
Chilengue <chilenguejoseorlando@gmail.com>
2024-01-06 02:44:41 +0200

    first commit

---
+++ js/js.js
@@ -0,0 +1,13 @@
+const counter =document.querySelector('#countervalue')
+const buttonDecrement =document.querySelector('#buttonDecrement')
+const buttonIncrement =document.querySelector('#buttonIncrement')
+
+let value=counter.value;
+buttonIncrement.addEventListener('click', ()=>{
+    value= ++value;
+    counter.value=value;
+})
+buttonDecrement.addEventListener('click', ()=>{
+    value =value !==0 ? --value :0;
+    counter.value =value;
+})
